ce24800b50f04db0489d75cadb1af79c9f5719a6
princekelvx <princekelvx@gmail.com>
2022-07-02 11:17:01 -0700

    first batch correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,5 @@
 
 
 def no_c(my_string):
-    string_list = list(my_string)
-    for i in range(len(string_list)):
-        if string_list[i] == 'c' or string_list[i] == 'C':
-            string_list[i] = ""
-    my_string = "".join(string_list)
-    return my_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
