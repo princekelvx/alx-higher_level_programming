334797eb5a0fcff41a2cf99d92a7df93d7f877b9
princekelvx <princekelvx@gmail.com>
2022-07-02 11:25:48 -0700

    added end guard

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -11,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,3 +21,5 @@ void free_listint(listint_t *head);
 
 void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
+
+#endif /*LISTS_H*/
