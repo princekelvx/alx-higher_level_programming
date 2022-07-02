84ad7c028a0fca63d26303ad75748f72054ab410
princekelvx <princekelvx@gmail.com>
2022-07-02 13:52:12 -0700

    fixed typo

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,7 +8,7 @@ void print_python_list_info(PyObject *p)
 	int i;
 	PyListObject *obj = (PyListObject *)p;
 
-	printf("[*] Size of the Python list = %li\n", size);
+	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
