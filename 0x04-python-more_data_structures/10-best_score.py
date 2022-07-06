eff8c4b4753e1793248911c39df1fc52e7d0aae5
princekelvx <princekelvx@gmail.com>
2022-07-06 06:05:21 -0700

    fixed pycodestyle issues

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    keymax = max(a_dictionary, key= lambda x: a_dictionary[x])
+    keymax = max(a_dictionary, key=lambda x: a_dictionary[x])
     return keymax
