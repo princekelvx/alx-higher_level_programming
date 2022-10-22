2fd6e81c10e9ed6f948dc84aa5fd298a09288ca5
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-10-22 09:38:51 -0700

    fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Send a get request with custom set HEADER variable
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
